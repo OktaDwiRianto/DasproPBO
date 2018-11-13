e21ec6dd95b028d54cda920f992716d16c1da9ae
OktaDwiRianto <35322043+OktaDwiRianto@users.noreply.github.com>
2018-11-13 20:09:31 +0700

    Update PemilikHP.java

--- PemilikHP.java
+++ PemilikHP.java
@@ -1,15 +1,4 @@
 package TugasPBO;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ACER
- */
 public class PemilikHP {
     String nama;
     String alamat;
